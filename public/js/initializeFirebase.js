8e573347063fb90e144214e36bccb3213b9a4285
ZedTT <ZackThomasTaylor@gmail.com>
2019-04-12 18:15:16 -0700

    Moved js files around and beautified code (indentation)

--- public/js/initializeFirebase.js
+++ public/js/initializeFirebase.js
@@ -1,10 +1,10 @@
-        // Initialize Firebase
-        var config = {
-            apiKey: "AIzaSyBwSxJojnYNo9ZFKHmO59sKVPdEhCR641A",
-            authDomain: "comp1930database.firebaseapp.com",
-            databaseURL: "https://comp1930database.firebaseio.com",
-            projectId: "comp1930database",
-            storageBucket: "comp1930database.appspot.com",
-            messagingSenderId: "908131063499"
-        };
-        firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBwSxJojnYNo9ZFKHmO59sKVPdEhCR641A",
+    authDomain: "comp1930database.firebaseapp.com",
+    databaseURL: "https://comp1930database.firebaseio.com",
+    projectId: "comp1930database",
+    storageBucket: "comp1930database.appspot.com",
+    messagingSenderId: "908131063499"
+};
+firebase.initializeApp(config);
